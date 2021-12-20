d6397556b4984cecef81a946a83ff66be0e1a02b
dzozrawr <jovan_96@live.ru>
2021-12-20 15:00:36 +0100

    added singe projectile shooting mechanic

---
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float shootingPower = 5f;
+
+    public GameObject arrow;
+    public Transform shootPoint;
+
+    Vector3 shootPointDefaultRot;
+
+    public float aimingRotSpeed = 20f;
+    private float rotLimit = -70f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootPointDefaultRot = shootPoint.localRotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float newXVal = shootPoint.rotation.eulerAngles.x - aimingRotSpeed * Time.deltaTime;
+
+            if (((newXVal-360f) < rotLimit) && ((newXVal - 360f)>-360f)) newXVal = rotLimit;
+
+            
+
+            shootPoint.rotation = Quaternion.Euler(newXVal, shootPoint.rotation.eulerAngles.y, shootPoint.rotation.eulerAngles.z);
+
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject newArrow = Instantiate(arrow, shootPoint.position, shootPoint.rotation);
+            newArrow.GetComponent<Rigidbody>().velocity = shootPoint.transform.forward * shootingPower;
+
+            shootPoint.rotation = Quaternion.Euler(shootPointDefaultRot);
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public Transform com;
+    public Rigidbody rb;
+
+    private float rotationSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //   rb = GetComponent<Rigidbody>();
+        //  rb.centerOfMass = com.position;
+
+        // transform.rotation = Quaternion.LookRotation(rb.velocity);
+
+        if (rb.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(rb.velocity),
+                Time.deltaTime * rotationSpeed
+            );
+            // Debug.DrawLine(transform.position, rb.);
+        }
+    }
+}
